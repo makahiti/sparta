b6579040f7e06c4fdf91e6310f989840886b6cce
makahiti <bmlee3661@gmail.com>
2026-02-11 01:15:48 +0900

    2

--- 1983.조교의 성적 매기기.py
+++ 1983.조교의 성적 매기기.py
@@ -1,18 +1,23 @@
-T=int(input())
-
-for t in range(1, T+1):
-
-# N은 학생 수,  K는 학점을 알고 싶은 학생의 번호
+T = int(input())
+for tc in range(1, T+1):
     N, K = map(int, input().split())
-    S=[list(map(int,input().split())) for _ in range(N)]
-
-    i=len(S)
-    j=len(S[0])
-    
-    #총점 구하기 -> 학점으로 만들기 -> 
-    for i in range(N):
-            score=0
-                score=S[i][0]*(35/100)+S[i][1]*(45/100)+S[i][2]*(20/100)
-                if S[i] > 
-
-        
+    student_score = [list(map(int, input().split())) for _ in range(N)]
+    grade = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+ 
+    lst = [] # 모든 학생의 총점
+    K_score = 0 # K번 학생 점수
+    for i in range(len(student_score)): # 점수 계산
+        s = student_score[i]
+        total_score = s[0]*0.35 + s[1]*0.45 + s[2]*0.2
+        lst.append(total_score)#학생들의 점수를 리스트에 추가
+ 
+        if i == K-1: #K는 1번부터, 리스트 인덱스는 0부터, K=3 == i[2]
+            K_score = total_score
+ 
+    lst.sort(reverse=True) # 점수 내림차순 정렬
+    K_idx = lst.index(K_score) # K 번 학생 위치 구하기 .index 메소드를 통해 처음 등장하는 인덱스 반환
+ 
+    K_grade = grade[K_idx//(N//10)] #N//10 = 한 학점당 들어갈 학생 수
+                                    #grade 리스트 안에서 인덱스 호출
+ 
+    print(f'#{tc} {K_grade}')
