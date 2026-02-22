48de43c4db4b1918f4725d84efb67a497a547a29
makahiti <bmlee3661@gmail.com>
2026-02-22 21:29:12 +0900

    2020_2

--- 0220/1289. 원재의 메모리 복구하기.py
+++ 0220/1289. 원재의 메모리 복구하기.py
@@ -1,5 +1,12 @@
 T = int(input())
 
 for tc in range(1, T+1):
-    memory=list(input())
-    count=
+    memory=list(map(int,input()))
+    count=0 #수정 되는 횟수
+    current=0 #0으로 고쳐야함
+
+    for i in memory:
+        if i != current: #i가 0이 아니면
+            count+=1 #수정 횟수 +1
+            current=i #바뀐 값으로 수정
+    print(f'#{tc} {count}')
---
+++ 0220/20551. 증가하는 사탕 수열.py
@@ -0,0 +1,20 @@
+# T = int(input())
+
+# for tc in range(1, T + 1):
+#     A, B, C = map(int, input().split())
+
+    
+#     c = C
+#     b = # B는 C보다 +1
+#     # A는 b-1 이하로만 가능
+#     a = 
+
+    
+#     if a < 1:
+
+#     else
+        
+# #아웃풋 조건을 어떻게 맞춰야하는지 모르겠습니다 ㅠㅜ
+        
+
+   
