4010f1b10e7a416aaa9c4deb80f0c22fafa8201a
bongmin <bmlee3661@gmail.com>
2026-02-24 17:57:55 +0900

    0224_1

---
+++ 0224/23795.우주 괴물.py
@@ -0,0 +1,13 @@
+T=int(input())
+
+for tc in range(1,T+1):
+    N=int(input())
+    arr=[list(map(int, input().split()))for _ in range(N)]
+    
+    di=[0,0,-1,1]
+    dj=[-1,1,0,0]    
+    
+    for i in range():
+        for j in range():
+            
+    
