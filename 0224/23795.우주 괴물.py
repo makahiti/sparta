ae1e99925fa1a522e4a87d745d88b723d4f7f02b
makahiti <bmlee3661@gmail.com>
2026-02-24 21:26:20 +0900

    0224_2

--- 0224/23795.우주 괴물.py
+++ 0224/23795.우주 괴물.py
@@ -1,13 +1,36 @@
-T=int(input())
+T = int(input())
 
-for tc in range(1,T+1):
-    N=int(input())
-    arr=[list(map(int, input().split()))for _ in range(N)]
-    
-    di=[0,0,-1,1]
-    dj=[-1,1,0,0]    
-    
-    for i in range():
-        for j in range():
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # 상, 하, 좌, 우
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
+
+    beam = 1
+    wall = 0
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                wall += 1 #벽 개수 카운트
+
             
-    
+            if arr[i][j] == 2:
+                xi, xj = i, j
+
+                # 상하좌우 레이저
+                for d in range(4):
+                    for c in range(1, N):
+                        ni = xi + di[d] * c
+                        nj = xj + dj[d] * c
+
+                        if 0 <= ni < N and 0 <= nj < N:
+                            if arr[ni][nj] == 0:
+                                beam += 1 #빔 카운트
+                            else:
+                                break
+
+    safe = N * N - (wall + beam)
+    print(f'#{tc} {safe}')
